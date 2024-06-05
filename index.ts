a95652bba095993a9c70be711a025fa3847421ba
Ty Mason <hi@tygr.dev>
2024-06-06 00:04:18 +0100

    feat: create ping command

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
-import { Client, Events } from "discord.js"
+import { Client, Events, GatewayIntentBits } from "discord.js"
 
-const client = new Client({ intents: [] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.once(Events.ClientReady, (c) => {
 	console.log("Ready! Logged in as", c.user.tag)
+});
+
+client.on(Events.MessageCreate, (msg) => {
+	if (!msg.content.startsWith(Bun.env.PREFIX as string) || msg.author.bot) return;
+
+	if (msg.content.startsWith(Bun.env.PREFIX as string + "ping")) {
+		// Ping Command
+		msg.channel.send("?ping");
+	}
 });
 
 client.login(Bun.env.DISCORD_TOKEN);
