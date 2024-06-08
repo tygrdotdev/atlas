f76f8ecf27be5adaf66c1d9923ef3f006dadb3f0
Ty Mason <hi@tygr.dev>
2024-06-08 01:13:35 +0100

    feat: add volume command

---
+++ src/commands/music/volume.ts
@@ -0,0 +1,23 @@
+import { DiscordCommand } from "../../types/command";
+
+export const command: DiscordCommand = {
+	name: "volume",
+	description: "Set the music volume.",
+	category: "Music",
+	cmd: async (client, msg, args) => {
+		if (!args[0]) return msg.channel.send(`Please provide a new volume to set. For example: \`${client.prefix}volume 35\``);
+
+		let player = client.kazagumo.getPlayer(msg.guild?.id as string);
+
+		if (!player) return msg.channel.send("There is nothing playing.")
+
+		if (!msg.member?.voice.channelId) return msg.channel.send("You are not in a voice channel.")
+		if (msg.member.voice.channelId !== player.voiceId) return msg.channel.send("You are not in the same voice channel as me!");
+
+		const volume = parseInt(args[0]);
+
+		player.setVolume(volume);
+
+		return msg.channel.send(`Set the volume to \`${volume}\`.`);
+	}
+}
