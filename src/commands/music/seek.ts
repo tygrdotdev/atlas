1f110e8a87ea7d759c21ed8125d9b56846fbe782
Ty Mason <hi@tygr.dev>
2024-06-14 20:00:33 +0100

    feat: add seek command

---
+++ src/commands/music/seek.ts
@@ -0,0 +1,26 @@
+import { formatDuration } from "../../lib/utils";
+import { DiscordCommand } from "../../types/command";
+
+export const command: DiscordCommand = {
+	name: "seek",
+	description: "Seek through the currently playing track.",
+	category: "music",
+	aliases: [],
+	cmd: async (client, msg, args) => {
+		let player = client.kazagumo.getPlayer(msg.guild?.id as string);
+
+		if (!player) return msg.channel.send("There is no player.");
+		if (!player.queue.current) return msg.channel.send("There is nothing playing!");
+
+		if (!msg.member?.voice.channelId) return msg.channel.send("You are not in a voice channel.")
+		if (msg.member.voice.channelId !== player.voiceId) return msg.channel.send("You are not in the same voice channel as me!");
+
+		if (!args[0]) return msg.channel.send(`Please specify how many seconds you would like to seek. Example: \`${client.prefix}seek 30\``);
+
+		const ms = parseInt(args[0]) * 1000;
+
+		await player.seek(ms);
+
+		return msg.channel.send(`Seeked the current track to: \`${formatDuration(ms)}\``);
+	}
+}
