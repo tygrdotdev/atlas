a2b4f9d69fdcaa7d3a69d49e747051805a1ad9ed
Ty Mason <hi@tygr.dev>
2024-06-19 03:10:00 +0100

    fix(shoukaku): check reason length instead of type

--- src/events/shoukaku/close.ts
+++ src/events/shoukaku/close.ts
@@ -3,6 +3,6 @@ import { ShoukakuEvent } from "../../types/shoukaku";
 export const event: ShoukakuEvent<"close"> = {
 	name: "close",
 	cmd: (client, name, code, reason) => {
-		client.log.shoukaku.warn(`Connection lost: ${reason || 'No reason'}`, false);
+		client.log.shoukaku.warn(`Connection lost: ${reason.length >= 1 ? reason : 'No reason'}`, false);
 	}
 }
