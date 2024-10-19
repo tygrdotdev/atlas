734476297937ae2587b117b734f135d063e84937
tygrxqt <hi@tygr.dev>
2024-10-19 15:19:08 +0100

    feat: update shoukaku error handling

--- src/events/shoukaku/error.ts
+++ src/events/shoukaku/error.ts
@@ -3,6 +3,11 @@ import { ShoukakuEvent } from "../../types/shoukaku";
 export const event: ShoukakuEvent<"error"> = {
 	name: "error",
 	cmd: (client, name, error) => {
+		if (error instanceof AggregateError) {
+			error.errors.forEach((e) => {
+				client.log.shoukaku.error(`Error: ${e}`);
+			})
+		}
 		client.log.shoukaku.error(`Error: ${error}`);
 	}
 }
